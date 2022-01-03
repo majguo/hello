fa56585e632ace5e8b05f7d0982059d97e7868c2
Jianguo Ma <jiangma@microsoft.com>
2022-01-04 07:52:43 +0800

    add another app which references the local module

--- hello.go
+++ hello.go
@@ -11,13 +11,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Jianguo")
+	message, err := greetings.Hello("Z")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
 
-	names := []string{"Alan", "Yan", "Jianguo"}
+	names := []string{"X", "Y", "Z"}
 	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
